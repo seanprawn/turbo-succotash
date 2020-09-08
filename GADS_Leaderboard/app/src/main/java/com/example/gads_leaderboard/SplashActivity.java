e1444a87146fe1833cccae4c6397c3099c8ff6aa
Sean Liebenberg <seansound@gmail.com>
2020-09-08 22:54:58 +0200

    added App Bar and some styling

--- GADS_Leaderboard/app/src/main/java/com/example/gads_leaderboard/SplashActivity.java
+++ GADS_Leaderboard/app/src/main/java/com/example/gads_leaderboard/SplashActivity.java
@@ -3,14 +3,29 @@ package com.example.gads_leaderboard;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final int SPLASH_DISPLAY_LENGTH = 1000;
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(SplashActivity.this, MainActivity.class));
-        finish();
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Main-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
-}
+//        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+//        finish();
+    }
+
+
